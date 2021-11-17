7071e18e63bd73b83eab0ad3cb80cd61078c0c25
RogerioPoturlhakLeal <rogerio.guitar@live.com>
2021-11-16 21:29:11 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -8,103 +8,106 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_products")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-		
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		private String name;
-		private String description;
-		private Double price;
-		private String imgUrl;
-		
-		@Transient
-		private Set<Category> categories = new HashSet<>();
-		
-		public Product() {			
-		}
 
-		public Product(Long id, String name, String description, Double price, String imgUrl) {
-			super();
-			this.id = id;
-			this.name = name;
-			this.description = description;
-			this.price = price;
-			this.imgUrl = imgUrl;
-		}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 
-		public Long getId() {
-			return id;
-		}
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+	public Product() {
+	}
 
-		public String getName() {
-			return name;
-		}
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	public Long getId() {
+		return id;
+	}
 
-		public String getDescription() {
-			return description;
-		}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-		public void setDescription(String description) {
-			this.description = description;
-		}
+	public String getName() {
+		return name;
+	}
 
-		public Double getPrice() {
-			return price;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public void setPrice(Double price) {
-			this.price = price;
-		}
+	public String getDescription() {
+		return description;
+	}
 
-		public String getImgUrl() {
-			return imgUrl;
-		}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-		public void setImgUrl(String imgUrl) {
-			this.imgUrl = imgUrl;
-		}
+	public Double getPrice() {
+		return price;
+	}
 
-		public Set<Category> getCategories() {
-			return categories;
-		}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			return result;
-		}
+	public String getImgUrl() {
+		return imgUrl;
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Product other = (Product) obj;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
 			return true;
-		}		
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
